34ddacaac158eca6927261f516d06fb4b5af660e
saksham-kapoor <sakshamkapoor1729@gmail.com>
2020-07-29 17:17:28 +0530

    Animating an element

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Exploring Framer Motion
+# Framer Motion Notes
+
+## Animating An Element
+
+Let element be a button:
+
+```html
+<button>Click Me!</button>
+```
+
+To simply animate it, import framer motion, then apply it the button like so
+
+```js
+import { motion } from "framer-motion";
+
+<motion.button animate={{ fontSize: 50 }}>Click Me!</motion.button>;
+```
+
+The button will automatically animate to a font-size of 50px on page load.
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const Home = () => {
   return (
-    <div className='home container'>
+    <motion.div className='home container' animate={}>
       <h2>Welcome to Pizza Joint</h2>
       <Link to='/base'>
-        <button>Create Your Pizza</button>
+        <motion.button animate={}>Create Your Pizza</motion.button>
       </Link>
-    </div>
+    </motion.div>
   );
 };
 
